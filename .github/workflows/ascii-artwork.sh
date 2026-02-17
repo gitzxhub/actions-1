82a8c9d4876b523725caaed58b8c048e0bc96fb7
gitzxhub <vivek6ece@gmail.com>
2026-02-17 18:42:14 +0530

    with added permissions-1

--- .github/workflows/ascii-artwork.sh
+++ .github/workflows/ascii-artwork.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#! /bin/sh
 
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run your code, I am dragon .... RAWRA" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
-ls
+ls -ltra
 
 
