860426d264035b02e83fc94537224a21a207ccd4
gitzxhub <vivek6ece@gmail.com>
2026-02-17 19:16:34 +0530

    file name changed to ascii-script

--- .github/workflows/ascii-artwork.sh
+++ .github/workflows/ascii-artwork.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-sudo apt-get install cowsay -y
-cowsay -f dragon "Run your code, I am dragon .... RAWRA" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-ls -ltra
-
 
---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run your code, I am dragon .... RAWRA" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
+
